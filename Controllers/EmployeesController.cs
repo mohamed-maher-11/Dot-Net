dee6aa9740696481bd05c375029cc2cfd816ef1a
zmohamed11 <m.mohamed57875@gmail.com>
2025-10-06 17:37:00 +0300

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        static List <string> Names = new List<string>() { "Ahmed", "Mohamed", "Ali" };
+        //int index = Names.Count;
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    return "Welcome";
+        //}
+        [HttpGet]
+        public IEnumerable<string> GetName()
+        {
+            return Names;
+        }
+
+        [HttpGet("{ID}")]
+        public string GetByID(int ID)
+        {
+            return Names[ID];
+        }
+
+        [HttpPost]
+        public string Post(string newName)
+        {
+            Names.Add(newName);
+            return Ok().ToString();
+        }
+
+        [HttpPut]
+        public string Put(int ID, string updateName)
+        {
+            Names[ID] = updateName;
+            return NoContent().ToString();
+        }
+
+        [HttpDelete]
+        public string Delete (int ID)
+        {
+            Names.RemoveAt(ID);
+            return NoContent().ToString();
+        }
+    }
+}
